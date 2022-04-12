1a9e09758ff3d793f366c50c683e6e2120be77b0
Dustin Bahr <dustin@curiousmedia.com>
2022-04-12 09:55:46 -0600

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-native-speech-commands
+
+React Native wrapper for tflite speech commands example
+
+## Installation
+
+```sh
+npm install react-native-speech-commands
+```
+
+## Usage
+
+```js
+import { multiply } from "react-native-speech-commands";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/reactnativespeechcommands/SpeechCommandsPackage.java
@@ -0,0 +1,28 @@
+package com.reactnativespeechcommands;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SpeechCommandsPackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new SpeechCommandsModule(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
